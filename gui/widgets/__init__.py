336878f574cef3092b4f7771778892e0962c9fbd
sudharshan-saranathan <sudharshan.saranathan@outlook.in>
2026-01-15 22:42:39 +0530

    refactoring

--- gui/widgets/__init__.py
+++ gui/widgets/__init__.py
@@ -1,5 +1,6 @@
 from .toolbar import ToolBar
 from .layouts import GLayout, HLayout, VLayout
 from .lights import Lights
+from .viewer import Viewer
 
-__all__ = ["GLayout", "HLayout", "VLayout", "ToolBar", "Lights"]
+__all__ = ["GLayout", "HLayout", "VLayout", "ToolBar", "Lights", "Viewer"]
