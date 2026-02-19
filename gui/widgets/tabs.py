74d7679dcbf0e04adf4be0bde736434f31e32065
Sudharshan Saranathan <sudharshan.saranathan@outlook.in>
2026-02-19 23:13:39 +0530

    Refactoring

--- gui/widgets/tabs.py
+++ gui/widgets/tabs.py
@@ -12,6 +12,9 @@ from PySide6 import QtGui
 from PySide6 import QtCore
 from PySide6 import QtWidgets
 
+# Climact modules: gui.widgets
+from gui.widgets.toolbar import ToolBar
+
 
 class TabWidget(QtWidgets.QTabWidget):
     """
@@ -49,6 +52,16 @@ class TabWidget(QtWidgets.QTabWidget):
             tabsClosable=self._attrs.closable,
         )
 
+        # Define a widget factory list
+        self._widget_factory: dict[type, tuple] = {}
+
+        # Initialize corner toolbar
+        self._corner_toolbar = ToolBar(
+            parent=self,
+            trailing=True,
+            iconSize=QtCore.QSize(16, 16),
+        )
+
     def _initialize_defaults(self) -> None:
 
         self._theme = self.DefaultTheme(
@@ -62,6 +75,15 @@ class TabWidget(QtWidgets.QTabWidget):
             movable=True,
             background=QtGui.QBrush(QtGui.QColor(0x232A2E)),
         )
+
+    def register_widget(self, _widget: type, _icon: QtGui.QIcon, _label: str):
+
+        action = QtGui.QAction(_label, icon=_icon, parent=self._corner_toolbar)
+        action.triggered.connect(lambda: self.addTab(_widget()))
+        action.setObjectName(_label)
+
+        self._widget_factory[_widget] = (_icon, _label)
+        self._corner_toolbar.addAction(action)
 
     def addTab(
         self,
